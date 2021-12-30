63a6aa9b1ea98642eebce6b71741a389da948771
Ambarish Patil <ambarish.patil@Indegene.com>
2021-12-30 12:23:57 +0530

    logo updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import Profile from "./components/Profile/Profile";
 import Intro from "./components/Intro/OneCol";
 import About from "./components/About/OneCol";
